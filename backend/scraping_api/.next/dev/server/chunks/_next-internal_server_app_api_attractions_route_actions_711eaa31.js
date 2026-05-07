d93e23464617c3cbaceaf67921c84d7032759bc4
JesseBui <khang.bui211204@gmail.com>
2026-05-07 10:25:34 -0400

    ignore scraping api built output

--- backend/scraping_api/.next/dev/server/chunks/_next-internal_server_app_api_attractions_route_actions_711eaa31.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/attractions/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_attractions_route_actions_711eaa31.js.map
